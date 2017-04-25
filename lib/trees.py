e553c24a45b22cdefed09910aeefc67f0a1bca9d
Pawel Szalachowski <psz@inf.ethz.ch>
2017-04-25 17:01:00 +0200

    TreeEntry, BaseTree

--- lib/trees.py
+++ lib/trees.py
@@ -11,4 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import merkle
+
+# External
+from merkle import MerkleTree, Node
+
+class TreeEntry(object):
+    def __init__():
+        self.added = None
+
+    def get_data_to_hash(self):
+        return b""
+
+    def get_label(self):
+        return None
+
+
+class BaseTree(MerkleTree):
+    def __init__(entries=None, sort=False):
+        self.entries = []
+        if entries is not None:
+            self.entries = entries
+        self.sort = sort
+        if self.sort:
+            self.entries.sort()
+        # Build an actual tree
+        leaves = []
+        for entry in entries:
+            leaves.append(entry.get_data_to_hash())
+        super().__init__(leaves)
+
+    def add(self, entry):
+        if self.sort:
+            # find position and insert (entries + leaves)
+            pass
+        else:
+            self.entries.append(entry)
+            self.leaves.append(Node(entry.get_data_to_hash()))
+
+    def add_hash(self, value):  # May be confusing, don't implement
+        raise NotImplementedError
