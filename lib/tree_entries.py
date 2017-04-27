8c9fef3ae41eb34672456d24d818c8811ad76a80
Pawel Szalachowski <psz@inf.ethz.ch>
2017-04-27 11:00:37 +0200

    trees, trees, ...

--- lib/tree_entries.py
+++ lib/tree_entries.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# External
 from merkle import hash_function
 
 from pki.lib.utils import dict_to_json
--- lib/trees.py
+++ lib/trees.py
@@ -11,15 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Stdlib
 import bisect
 import logging
 
-# External
-from merkle import MerkleTree, Node
+from merkle import MerkleTree, Node, hash_function
 
-# EEPKI
 from .tree_entries import (
     CertificateEntry,
     MSCEntry,
@@ -29,43 +25,26 @@ from .tree_entries import (
     )
 
 
-
 class BaseTree(MerkleTree):
-    def __init__(self, entries=None, sort=False):
+    def __init__(self, entries=None):
         self.entries = []
         if entries is not None:
             self.entries = entries
-        self.sort = sort
-        if self.sort:
-            self.entries.sort()
         # Build an actual tree
         leaves = []
         for entry in entries:
             leaves.append(entry.get_data())
         super().__init__(leaves)
 
-    def add(self, entry):
-        if self.sort:  # Leaves of the tree are sorted
-            index = bisect.bisect_left(self.entries, entry)
-            if self.entries and self.entries[index] == entry:
-                logging.info("Replacing entry: %s by %s" % (self.entries[index], entry))
-                self.entries[index] = entry
-                self.leaves[index] = Node(entry.get_data())
-            else:
-                self.entries.insert(index, entry)
-                self.leaves.insert(index, Node(entry.get_data()))
-        else:  # Append-only tree
-            self.entries.append(entry)
-            self.leaves.append(Node(entry.get_data()))
+    def get_root(self):
+        if self.root:
+            return self.root.val
+        return hash_function(b"").digest()
 
-    def get_proof(self, entry):
-        raise NotImplementedError
-
-    def get_presence_proof(self, entry):
-        raise NotImplementedError
-
-    def get_absence_proof(self, entry):
-        assert self.sort
+    def get_proof(self, index):
+        if 0 <= index < len(self.leaves):
+            return self.get_chain(index)
+        return None
 
     def add_hash(self, value):  # Not needed and may be confusing, don't implement
         raise NotImplementedError
@@ -74,16 +53,58 @@ class BaseTree(MerkleTree):
         raise NotImplementedError
 
 
-class ConsistencyTree(BaseTree):
+class ApendOnlyTree(BaseTree):
+    def add(self, entry):
+        self.entries.append(entry)
+        self.leaves.append(Node(entry.get_data()))
+
+
+class SortedTree(BaseTree):
+    def __init__(self, entries=None):
+        tmp = []
+        if entries:
+            tmp = sorted(entries)
+        super().__init__(tmp)
+
+    def add(self, entry):
+        index = self.get_idx_for_entry(entry)
+        if self.entries and self.entries[index] == entry:
+            logging.info("Replacing entry: %s by %s" % (self.entries[index], entry))
+            self.entries[index] = entry
+            self.leaves[index] = Node(entry.get_data())
+        else:
+            self.entries.insert(index, entry)
+            self.leaves.insert(index, Node(entry.get_data()))
+
+    def get_idx_for_entry(self, entry):
+        bisect.bisect_left(self.entries, entry)
+
+    def get_idx_for_hash(self, hash_):
+        bisect.bisect_left(self.leaves, hash_)
+
+    def get_proof(self, entry):
+        presence = self.get_presence_proof(entry)
+        if presence:
+            return presence
+        return self.get_absence_proof(entry)
+
+    def get_presence_proof(self, entry):
+        raise NotImplementedError
+
+    def get_absence_proof(self, entry):  # Can be implemented only by sorted trees
+        raise NotImplementedError
+
+
+class ConsistencyTree(AppendOnlyTree):
     """
     Tree that contains all object in chronological order. See Section 5.3 and
     Figure 5 from the PoliCert paper.
     """
-    def __init__(self, entries=None):
-        super().__init__(entries)  # Don't sort
+    def get_presence_proof(self, entry):
+        raise NotImplementedError
 
 
-class CertificateTree(BaseTree):
+class CertificateTree(SortedTree):
     """
     Tree that contains all certificates and their (optional) revocations.
     Entries of the tree are sorted. See Section 5.3 and Figure 3 from the
@@ -100,7 +121,7 @@ class CertificateTree(BaseTree):
         raise NotImplementedError
 
 
-class PolicySubTree(BaseTree):
+class PolicySubTree(SortedTree):
     """
     Tree that contains all policies for a given domain level (e.g., all policies
     of X.a.com, or all TLD policies). Entries of the tree are sorted. See
